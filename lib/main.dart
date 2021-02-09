3f41b1208d46b221936d30b6699ebcddb5978f84
ahmed <serga2050@gmail.com>
2021-02-09 22:49:38 +0200

    shopping

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //Shop
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.blue,    
+       primarySwatch: Colors.indigo
       ),
       home: Home(),
     );
